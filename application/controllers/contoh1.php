8f76bed7af3c8770dcb92367ec3665b27149d164
wbornean <wbornean@gmail.com>
2022-09-19 21:08:27 +0700

    .

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya farid putra ramadani
+            Saya tinggal di Kulonprogo
+            Kegiatan yang saya sukai adalah
+            Rebahan";
+    }
+}
